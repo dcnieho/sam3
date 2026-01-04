96badb66713e3614b8b880295fb65f2f6cee6ff9
Diederick Niehorster <dcnieho@gmail.com>
2026-01-04 10:45:53 +0100

    only add ffmpeg to path if it is not on path already

--- sam3/model/utils/misc.py
+++ sam3/model/utils/misc.py
@@ -130,7 +130,8 @@ class TorchCodecDecoder:
     def __init__(self, source, dimension_order="NCHW", device="cpu", num_threads=1):
         # ensure ffmpeg binaries needed by torchcodec are on path
         import ffmpeg as _ffmpeg
-        _ffmpeg.add_to_path()
+        if not _ffmpeg.is_on_path():
+            _ffmpeg.add_to_path()
 
         from torchcodec import _core as core
 
